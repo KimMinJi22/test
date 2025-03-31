408c86ed7747fe8a896b5513d450d2dfff2ecfee
KimMinJi22 <113487181+KimMinJi22@users.noreply.github.com>
2025-03-31 21:03:37 +0900

    Add files via upload

--- js/10000hours.js
+++ js/10000hours.js
@@ -54,15 +54,19 @@ window.onclick = function(event) {
 
 function copyUrl() {
     let url = window.location.href;
-    let tmp = document.createElement('input');
+    // 권장하지 않는 기능으로 수정.
+    // let tmp = document.createElement('input');
 
-    document.body.appendChild(tmp);
-    tmp.value = url;
-    tmp.select();
-    document.execCommand("copy");
-    document.body.removeChild(tmp);
+    // document.body.appendChild(tmp);
+    // tmp.value = url;
+    // tmp.select();
+    // document.execCommand("copy");
+    // document.body.removeChild(tmp);
+    // alert("URL이 복사되었습니다.");
 
-    alert("URL이 복사되었습니다.");
+    navigator.clipboard.writeText(url).then(() => {
+        alert("URL이 복사되었습니다.");
+    })
 }
 
 shareButton.addEventListener('click', copyUrl)
